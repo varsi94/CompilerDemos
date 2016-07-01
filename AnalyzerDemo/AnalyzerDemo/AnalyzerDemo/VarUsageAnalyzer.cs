9cb6753e14884d1203237799c609b2c37c51949c
Márton Varsányi <varsi.marci@gmail.com>
2016-07-01 23:00:17 +0200

    Analyzer Demo kiinduló

---
+++ AnalyzerDemo/AnalyzerDemo/AnalyzerDemo/VarUsageAnalyzer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AnalyzerDemo
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class VarUsageAnalyzer : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "VarUsage";
+		private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+		private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+		private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+		private const string Category = "Naming";
+
+		private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.LocalDeclarationStatement);
+		}
+
+		private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ AnalyzerDemo/AnalyzerDemo/AnalyzerDemo/VarUsageCodeFixProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+namespace AnalyzerDemo
+{
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(VarUsageCodeFixProvider)), Shared]
+	public class VarUsageCodeFixProvider : CodeFixProvider
+	{
+		private const string Title = "Change Var";
+
+		public sealed override ImmutableArray<string> FixableDiagnosticIds
+		{
+			get { return ImmutableArray.Create(VarUsageAnalyzer.DiagnosticId); }
+		}
+
+		public sealed override FixAllProvider GetFixAllProvider()
+		{
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+			var diagnostic = context.Diagnostics.First();
+			var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+			// Find the type declaration identified by the diagnostic.
+			var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<LocalDeclarationStatementSyntax>().First();
+
+			// Register a code action that will invoke the fix.
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: Title,
+					createChangedDocument: c => ChangeVarAsync(context.Document, declaration, c),
+					equivalenceKey: Title),
+				diagnostic);
+		}
+
+		private async Task<Document> ChangeVarAsync(Document document, LocalDeclarationStatementSyntax declaration, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
