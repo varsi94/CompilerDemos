4d5254be7968bccb297ce3cb5e6d3954a97fa54a
varsi94 <varsi.marci@gmail.com>
2016-07-31 21:52:50 +0200

    operator demo

---
+++ RewriterDemo/RewriterDemo/DiamondRewriter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RewriterDemo
+{
+    public class DiamondRewriter : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
+        {
+            var type = node.Declaration.Type as GenericNameSyntax;
+            if (type != null)
+            {
+                foreach (var variable in node.Declaration.Variables)
+                {
+                    var objCreation = variable.Initializer.Value as ObjectCreationExpressionSyntax;
+                    if (objCreation == null)
+                    {
+                        continue;
+                    }
+
+                    var nameSyntax = (objCreation.Type as GenericNameSyntax);
+                    if (nameSyntax == null)
+                    {
+                        continue;
+                    }
+
+                    if (!nameSyntax.TypeArgumentList.ChildNodes().OfType<OmittedTypeArgumentSyntax>().Any())
+                    {
+                        continue;
+                    }
+
+                    node = node.ReplaceNode(nameSyntax, nameSyntax.Update(nameSyntax.Identifier, type.TypeArgumentList));
+                }
+            }
+            return node;
+        }
+    }
+}
---
+++ RewriterDemo/RewriterDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RewriterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compilation test = CreateTestCompilation();
+
+            foreach (var syntaxTree in test.SyntaxTrees)
+            {
+                var diamondRewriter = new DiamondRewriter();
+                SyntaxNode newSource = diamondRewriter.Visit(syntaxTree.GetRoot());
+
+                if (newSource != syntaxTree.GetRoot())
+                {
+                    test = test.RemoveSyntaxTrees(syntaxTree);
+                    test = test.AddSyntaxTrees(newSource.SyntaxTree);
+                }
+            }
+
+            EmitResult result = test.Emit("dummy.exe");
+            Process.Start("dummy.exe");
+        }
+
+        static Compilation CreateTestCompilation()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+@"using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> list = new List<>() {1, 2, 3};
+			foreach (var x in list)
+			{
+				Console.WriteLine(x);
+			} 
+			Console.ReadLine();
+        }
+    }
+}");
+
+            MetadataReference mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            MetadataReference list = MetadataReference.CreateFromFile(typeof(List<>).Assembly.Location);
+
+            return CSharpCompilation.Create("Dummy", new[] { tree }, new[] { mscorlib, list },
+                new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+        }
+    }
+}
